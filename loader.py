d233e98676bb4e9cca95e1a3f89dec12a41eba3f
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-08-30 20:23:26 +0300

    Добавлено последовательное считывание информации о кандидатах из базы данных в формате Excel

--- loader.py
+++ loader.py
@@ -1,4 +1,26 @@
+import os.path
+import glob
+
 import configargparse
+from openpyxl import load_workbook
+
+
+def get_applicant_info_from_excel_database(base_path):
+    excel_filepath = glob.glob(os.path.join(base_path, '*.xlsx'))[0]
+
+    workbook = load_workbook(excel_filepath)
+
+    for worksheet in workbook:
+        for row_number, row_values in enumerate(worksheet.values):
+            if row_number > 0:
+                yield {
+                    'order_number': row_number,
+                    'vacancy': row_values[0],
+                    'full_name': row_values[1],
+                    'desired_salary': row_values[2],
+                    'comment': row_values[3],
+                    'status': row_values[4],
+                }
 
 
 def get_command_line_arguments():
@@ -26,6 +48,9 @@ def main():
 
     source_database_path = command_line_arguments.path
     huntflow_api_token = command_line_arguments.token
+
+    for applicant_info in get_applicant_info_from_excel_database(source_database_path):
+        pass
 
 
 if __name__ == '__main__':
